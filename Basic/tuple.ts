ec0b90844952a21a51579234ffae9c1bf2db01dc
shubhamElemensis <168633512+shubhamElemensis@users.noreply.github.com>
2024-05-21 05:44:53 +0000

    tuple

---
+++ Basic/tuple.ts
@@ -0,0 +1,11 @@
+let objt: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "shubham",
+  age: 20,
+  hobbies: ["gaming", "volleyball"],
+  role: [2, "author"],
+};
